437a490386065f00475d9094fdff0073c363e403
phillipfu96 <54783408+phillipfu96@users.noreply.github.com>
2024-10-08 11:41:09 +0200

    Update app.py
    Now map will directly open in the browser after executing app.py

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
-#from flask import Flask, render_template
+import time, socket
 from flask import Flask, render_template, request
+import webbrowser
 
 app = Flask(__name__)
 
@@ -11,10 +12,16 @@ def index():
 def camera_preview():
     cam_id = request.args.get('id')
     return render_template('index.html', cam_id=cam_id)
-
-
+# open in browser
+def open_in_browser():
+    time.sleep(2)
+    ip_address = socket.gethostbyname(socket.gethostname())
+    port = 5000
+    local_ip = f"http://{ip_address}:{port}"
+    webbrowser.open(local_ip)
 
 if __name__ == '__main__':
+    open_in_browser()
     app.run(host='0.0.0.0', port=5000, debug=True)
 
 
